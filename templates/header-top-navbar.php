ebf7899970a12ac526b8b4fc48a7a13fe469be42
Colin Waddell <mrcolin@gmail.com>
2013-09-12 14:20:32 +0100

    moved some stuff about to fix mobile nav

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,13 +1,13 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container">
     <div class="navbar-header">
+      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
