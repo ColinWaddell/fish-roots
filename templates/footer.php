b2bd77a9b7cfc8450b7aa8226cadf8595f897b94
Colin Waddell <mrcolin@gmail.com>
2013-09-12 23:03:42 +0100

    fixed responsiveness on footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,11 @@
 <footer>
   <div class="content-info container" role="contentinfo">
     <div class="row">
-      <div class="col-lg-4">
+      <div class="col-sm-4">
         <p>Andrew T Fish BDS MFDS RCS(Edin)<br/>Altrincham Dental Practice,<br />63 Manchester Road,<br />WA14 4RH,<br/><strong>0161 928 2886</strong></p>
         <p><a href="mailto:Contact@AndrewFish.co.uk">Contact@AndrewFish.co.uk</a></p>
       </div>
-      <div class="col-lg-8 business-logos">
+      <div class="col-sm-8 business-logos">
         <a target="_blank" href="http://www.straumann.co.uk/"><img src="<?php echo home_url('/assets/img/straumann.png'); ?>" alt=""></a>
         <br />
         <a target="_blank" href="http://www.geistlich-surgery.com/home_en.html"><img src="<?php echo home_url('/assets/img/geistlich-surgery.png'); ?>" alt=""></a>
