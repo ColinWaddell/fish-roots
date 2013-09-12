64dbc921c0eb31833c9aa82a6e923edce0ad1433
Colin Waddell <mrcolin@gmail.com>
2013-09-12 20:20:51 +0100

    removed some redundant code from homepage

--- templates/content-home.php
+++ templates/content-home.php
@@ -1,21 +1,17 @@
 
 
 <?php while (have_posts()) : the_post(); ?>
-  <figure class="home-carousel">
-    <div class="row">
+  <div class="row">
 
-      <div class="col-sm-4">
-        <div class="thumbnail">
-          <?php the_post_thumbnail(); ?>  
-        </div>
-      </div>  
+    <div class="col-sm-4">
+      <?php the_post_thumbnail(); ?>  
+    </div>  
 
-      <div class="col-sm-8">
-        <?php the_content(); ?>
-      </div>  
+    <div class="col-sm-8">
+      <?php the_content(); ?>
+    </div>  
 
-    </div>
-  </figure>
+  </div>
 <?php endwhile; ?>
 
 
