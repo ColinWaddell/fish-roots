846fe4d86ac408d67c79613233be8c2ba7c545fd
Colin Waddell <mrcolin@gmail.com>
2013-09-12 23:02:12 +0100

    fixed image width on homepage

--- templates/content-home.php
+++ templates/content-home.php
@@ -3,11 +3,11 @@
 <?php while (have_posts()) : the_post(); ?>
   <div class="row">
 
-    <div class="col-sm-4 home-thumbnail">
+    <div class="col-xs-4 home-thumbnail">
       <?php the_post_thumbnail(); ?>  
     </div>  
 
-    <div class="col-sm-8">
+    <div class="col-xs-8">
       <?php the_content(); ?>
     </div>  
 
