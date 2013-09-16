0a04173bcf696dd15aa252488b1288a6c8e92ef1
Colin Waddell <mrcolin@gmail.com>
2013-09-16 19:58:06 +0100

    added more logo's

--- templates/footer.php
+++ templates/footer.php
@@ -2,18 +2,51 @@
   <div class="content-info container" role="contentinfo">
     <div class="row">
       <div class="col-sm-4">
-        <p>Andrew T Fish BDS MFDS RCS(Edin)<br/>Altrincham Dental Practice,<br />63 Manchester Road,<br />WA14 4RH,<br/><strong>0161 928 2886</strong></p>
+        <p>Andrew T Fish BDS MFDS RCS(Edin)<br/>Altrincham Dental Practice,<br />
+        63 Manchester Road,<br />WA14 4RH,<br/><strong>0161 928 2886</strong></p>
         <p><a href="mailto:Contact@AndrewFish.co.uk">Contact@AndrewFish.co.uk</a></p>
       </div>
-      <div class="col-sm-8 business-logos">
+
+      <div class="col-sm-4 business-logos center">
 
         <hr class="visible-xs" />
 
-        <a target="_blank" href="http://www.straumann.co.uk/"><img src="<?php echo home_url('/assets/img/straumann.png'); ?>" alt=""></a>
+        <a target="_blank" href="http://www.straumann.co.uk/">
+          <img src="<?php echo home_url('/assets/img/straumann.png'); ?>" alt="">
+        </a>
+
         <br />
-        <a target="_blank" href="http://www.geistlich-surgery.com/home_en.html"><img src="<?php echo home_url('/assets/img/geistlich-surgery.png'); ?>" alt=""></a>
+
+        <a target="_blank" href="http://www.rcsed.ac.uk/">
+          <img src="<?php echo home_url('/assets/img/rcs_edinburgh.png'); ?>" alt="">
+        </a>
+
         <br />
-        <a target="_blank" href="http://www.geistlich.com/index.cfm?setlan=en"><img src="<?php echo home_url('/assets/img/geistlich-biomaterials.png'); ?>" alt=""></a>
+
+        <a target="_blank" href="http://www.adi.org.uk/">
+          <img src="<?php echo home_url('/assets/img/adi.png'); ?>" alt="">
+        </a>
+
+      </div>
+
+      <div class="col-sm-4 business-logos right">
+
+        <a target="_blank" href="http://www.geistlich.com/index.cfm?setlan=en">
+          <img src="<?php echo home_url('/assets/img/geistlich-biomaterials.png'); ?>" alt="">
+        </a>
+
+        <br />
+
+        <a target="_blank" href="http://www.nhs.uk/">
+          <img src="<?php echo home_url('/assets/img/nhs.png'); ?>" alt="">
+        </a>
+
+        <br />
+
+        <a target="_blank" href="http://www.iti.org/">
+          <img src="<?php echo home_url('/assets/img/iti.png'); ?>" alt="">
+        </a>
+
       </div>
     </div>
     
